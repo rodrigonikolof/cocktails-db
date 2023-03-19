dcdf54a0b2c00ec5ea338c7f6eb0334b4468a5a9
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-03-19 12:28:43 +1100

    Init commit

---
+++ src/context.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { useCallback } from 'react'
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = async() =>{
+    setLoading(true)
+    try{
+      const response = await fetch(`${url}${searchTerm}`).then(res => res.json()).then(data => console.log(data))
+      // const data = await response.json();
+      // console.log(data)
+    }
+    catch (error){
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchDrinks()
+  }, [searchTerm])
+
+  return (
+  <AppContext.Provider value={{
+    loading,  
+    cocktails, 
+    setSearchTerm
+    }}
+    >
+      {children}
+  </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, illum ad! Natus temporibus veniam, mollitia ut illum sit assumenda labore magnam, repudiandae debitis quas iusto officia aspernatur nam perspiciatis consequatur exercitationem ex quisquam cumque saepe harum adipisci? Corporis perspiciatis, molestias sequi doloremque, consequatur, vitae esse necessitatibus ad eius nostrum impedit.
+      </p>
+    </section>
+  )
+}
+
+export default About
