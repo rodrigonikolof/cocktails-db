de69cc3693f2e640b72f7de3f865f3fc354acf8b
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-03-21 10:00:09 +1100

    Added single page for each cocktail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import pages
 import Home from './pages/Home'
 import About from './pages/About'
