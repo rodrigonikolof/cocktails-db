6cdbae377377f391cde0be2bb76ad879fa49e7a3
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-03-20 11:48:17 +1100

    Fixed issued with fetching data

--- src/context.js
+++ src/context.js
@@ -12,12 +12,29 @@ const AppProvider = ({ children }) => {
   const fetchDrinks = async() =>{
     setLoading(true)
     try{
-      const response = await fetch(`${url}${searchTerm}`).then(res => res.json()).then(data => console.log(data))
-      // const data = await response.json();
-      // console.log(data)
+      const response = await fetch(`${url}${searchTerm}`)
+      const data = await response.json();
+      const {drinks} = data; 
+      if(drinks){
+        const newCocktails = drinks.map((item)=>{
+          const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = item; 
+          return { 
+            id: idDrink, 
+            name: strDrink, 
+            image: strDrinkThumb, 
+            info: strAlcoholic, 
+            glass: strGlass}
+        })
+        setCocktails(newCocktails)
+      }
+      else{
+        setCocktails([]);
+      }
+      setLoading(false)
     }
     catch (error){
       console.log(error)
+      setLoading(false)
     }
   }
 
